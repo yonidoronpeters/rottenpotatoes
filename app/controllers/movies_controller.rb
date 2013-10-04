a314a310ab908e09e62144bb0f155704b0681018
Yoni Doron-Peters <yonidoronpeters@berkeley.edu>
2013-10-04 01:04:02 -0700

    finish part 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,14 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @sort_by = params[:sort] || session[:sort]
+    if @sort_by == 'title'
+      @movies = Movie.order('title ASC')
+    elsif @sort_by == 'release_date'
+      @movies = Movie.order('release_date ASC')
+    else
+      @movies = Movie.all
+    end
   end
 
   def new
